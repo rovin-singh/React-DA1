3fb9b56eb744bd3a83a625b9a32a90f29e6e187f
rovin-singh <91409265+rovin-singh@users.noreply.github.com>
2022-05-19 11:53:08 +0530

    "changes occurs"

--- src/components/Mobile.jsx
+++ src/components/Mobile.jsx
@@ -1,5 +1,6 @@
 import React from "react-dom";
 import "./mobile.css";
+import { v4 as uuidv4 } from "uuid";
 const Mobile = () => {
   let data = ["Android", "BlackBerry", "iphone", "Windows Phone"];
   let mdata = ["Samsung", "HTC", "iphone", "Micromax"];
@@ -8,13 +9,13 @@ const Mobile = () => {
       <h3>Mobile Operating System</h3>
       <ul>
         {data.map((data) => (
-          <li>{data}</li>
+          <li key={uuidv4()}>{data}</li>
         ))}
       </ul>
       <h3>Mobile Manufacturers</h3>
       <ul>
         {mdata.map((data) => (
-          <li>{data}</li>
+          <li key={uuidv4()}>{data}</li>
         ))}
       </ul>
     </div>
